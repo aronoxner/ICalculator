c81729324e2b416b67e51a957e6e9d5d9c972f72
Aron Ochsner <aron.ochsner1@outlook.com>
2024-06-12 19:01:25 -0500

    Refactored set of variables, button inputs, and operations. Added switch cases to equals sign to perform the correct actions.

--- ICalculator/Form1.Designer.cs
+++ ICalculator/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            txtTitle = new TextBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BadCalculator));
             btnHistory = new Button();
             btnclearAll = new Button();
             btnClearRecent = new Button();
@@ -50,215 +50,273 @@
             btnPeriod = new Button();
             btnEquals = new Button();
             txtViewer = new TextBox();
+            txtTitle = new TextBox();
             SuspendLayout();
-            // 
-            // txtTitle
-            // 
-            txtTitle.Location = new Point(23, 12);
-            txtTitle.Name = "txtTitle";
-            txtTitle.Size = new Size(230, 23);
-            txtTitle.TabIndex = 0;
-            txtTitle.Text = "Calculator Title Goes Here";
-            txtTitle.TextAlign = HorizontalAlignment.Center;
             // 
             // btnHistory
             // 
+            btnHistory.BackColor = Color.Black;
+            btnHistory.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnHistory.ForeColor = SystemColors.Control;
             btnHistory.Location = new Point(23, 86);
             btnHistory.Name = "btnHistory";
             btnHistory.Size = new Size(53, 35);
             btnHistory.TabIndex = 1;
             btnHistory.Text = "History";
-            btnHistory.UseVisualStyleBackColor = true;
+            btnHistory.UseVisualStyleBackColor = false;
             // 
             // btnclearAll
             // 
+            btnclearAll.BackColor = Color.Black;
+            btnclearAll.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnclearAll.ForeColor = Color.White;
             btnclearAll.Location = new Point(82, 86);
             btnclearAll.Name = "btnclearAll";
             btnclearAll.Size = new Size(53, 35);
             btnclearAll.TabIndex = 2;
             btnclearAll.Text = "C";
-            btnclearAll.UseVisualStyleBackColor = true;
+            btnclearAll.UseVisualStyleBackColor = false;
             btnclearAll.Click += btnclearAll_Click;
             // 
             // btnClearRecent
             // 
+            btnClearRecent.BackColor = Color.Black;
+            btnClearRecent.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnClearRecent.ForeColor = Color.White;
             btnClearRecent.Location = new Point(141, 86);
             btnClearRecent.Name = "btnClearRecent";
             btnClearRecent.Size = new Size(53, 35);
             btnClearRecent.TabIndex = 3;
             btnClearRecent.Text = "CE";
-            btnClearRecent.UseVisualStyleBackColor = true;
+            btnClearRecent.UseVisualStyleBackColor = false;
             btnClearRecent.Click += btnClearRecent_Click;
             // 
             // btnDeleteDigit
             // 
+            btnDeleteDigit.BackColor = Color.Black;
+            btnDeleteDigit.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnDeleteDigit.ForeColor = Color.White;
             btnDeleteDigit.Location = new Point(141, 250);
             btnDeleteDigit.Name = "btnDeleteDigit";
             btnDeleteDigit.Size = new Size(53, 35);
             btnDeleteDigit.TabIndex = 4;
             btnDeleteDigit.Text = "DEL";
-            btnDeleteDigit.UseVisualStyleBackColor = true;
+            btnDeleteDigit.UseVisualStyleBackColor = false;
             btnDeleteDigit.Click += btnDeleteDigit_Click;
             // 
             // btn7
             // 
+            btn7.BackColor = Color.Black;
+            btn7.Font = new Font("Bell MT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn7.ForeColor = Color.Violet;
             btn7.Location = new Point(23, 127);
             btn7.Name = "btn7";
             btn7.Size = new Size(53, 35);
             btn7.TabIndex = 5;
             btn7.Text = "7";
-            btn7.UseVisualStyleBackColor = true;
+            btn7.UseVisualStyleBackColor = false;
             btn7.Click += btn7_Click;
             // 
             // btn8
             // 
+            btn8.BackColor = Color.Black;
+            btn8.Font = new Font("Bell MT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn8.ForeColor = Color.Violet;
             btn8.Location = new Point(82, 127);
             btn8.Name = "btn8";
             btn8.Size = new Size(53, 35);
             btn8.TabIndex = 6;
             btn8.Text = "8";
-            btn8.UseVisualStyleBackColor = true;
+            btn8.UseVisualStyleBackColor = false;
             btn8.Click += btn8_Click;
             // 
             // btn9
             // 
+            btn9.BackColor = Color.Black;
+            btn9.Font = new Font("Bell MT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn9.ForeColor = Color.Violet;
             btn9.Location = new Point(141, 127);
             btn9.Name = "btn9";
             btn9.Size = new Size(53, 35);
             btn9.TabIndex = 7;
             btn9.Text = "9";
-            btn9.UseVisualStyleBackColor = true;
+            btn9.UseVisualStyleBackColor = false;
             btn9.Click += btn9_Click;
             // 
             // btnDivide
             // 
+            btnDivide.BackColor = Color.Black;
+            btnDivide.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnDivide.ForeColor = Color.White;
             btnDivide.Location = new Point(200, 209);
             btnDivide.Name = "btnDivide";
             btnDivide.Size = new Size(53, 35);
             btnDivide.TabIndex = 8;
             btnDivide.Text = "÷";
-            btnDivide.UseVisualStyleBackColor = true;
+            btnDivide.UseVisualStyleBackColor = false;
+            btnDivide.Click += btnDivide_Click;
             // 
             // btn4
             // 
+            btn4.BackColor = Color.Black;
+            btn4.Font = new Font("Bell MT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn4.ForeColor = Color.Violet;
             btn4.Location = new Point(23, 168);
             btn4.Name = "btn4";
             btn4.Size = new Size(53, 35);
             btn4.TabIndex = 9;
             btn4.Text = "4";
-            btn4.UseVisualStyleBackColor = true;
+            btn4.UseVisualStyleBackColor = false;
             btn4.Click += btn4_Click;
             // 
             // btn5
             // 
+            btn5.BackColor = Color.Black;
+            btn5.Font = new Font("Bell MT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn5.ForeColor = Color.Violet;
             btn5.Location = new Point(82, 168);
             btn5.Name = "btn5";
             btn5.Size = new Size(53, 35);
             btn5.TabIndex = 10;
             btn5.Text = "5";
-            btn5.UseVisualStyleBackColor = true;
+            btn5.UseVisualStyleBackColor = false;
             btn5.Click += btn5_Click;
             // 
             // btn6
             // 
+            btn6.BackColor = Color.Black;
+            btn6.Font = new Font("Bell MT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn6.ForeColor = Color.Violet;
             btn6.Location = new Point(141, 168);
             btn6.Name = "btn6";
             btn6.Size = new Size(53, 35);
             btn6.TabIndex = 11;
             btn6.Text = "6";
-            btn6.UseVisualStyleBackColor = true;
+            btn6.UseVisualStyleBackColor = false;
             btn6.Click += btn6_Click;
             // 
             // btnMultiply
             // 
+            btnMultiply.BackColor = Color.Black;
+            btnMultiply.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnMultiply.ForeColor = Color.White;
             btnMultiply.Location = new Point(200, 168);
             btnMultiply.Name = "btnMultiply";
             btnMultiply.Size = new Size(53, 35);
             btnMultiply.TabIndex = 12;
             btnMultiply.Text = "X";
-            btnMultiply.UseVisualStyleBackColor = true;
+            btnMultiply.UseVisualStyleBackColor = false;
+            btnMultiply.Click += btnMultiply_Click;
             // 
             // btn1
             // 
+            btn1.BackColor = Color.Black;
+            btn1.Font = new Font("Bell MT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn1.ForeColor = Color.Violet;
             btn1.Location = new Point(23, 209);
             btn1.Name = "btn1";
             btn1.Size = new Size(53, 35);
             btn1.TabIndex = 13;
             btn1.Text = "1";
-            btn1.UseVisualStyleBackColor = true;
+            btn1.UseVisualStyleBackColor = false;
             btn1.Click += btn1_Click;
             // 
             // btn2
             // 
+            btn2.BackColor = Color.Black;
+            btn2.Font = new Font("Bell MT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn2.ForeColor = Color.Violet;
             btn2.Location = new Point(82, 209);
             btn2.Name = "btn2";
             btn2.Size = new Size(53, 35);
             btn2.TabIndex = 14;
             btn2.Text = "2";
-            btn2.UseVisualStyleBackColor = true;
+            btn2.UseVisualStyleBackColor = false;
             btn2.Click += btn2_Click;
             // 
             // btn3
             // 
+            btn3.BackColor = Color.Black;
+            btn3.Font = new Font("Bell MT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn3.ForeColor = Color.Violet;
             btn3.Location = new Point(141, 209);
             btn3.Name = "btn3";
             btn3.Size = new Size(53, 35);
             btn3.TabIndex = 15;
             btn3.Text = "3";
-            btn3.UseVisualStyleBackColor = true;
+            btn3.UseVisualStyleBackColor = false;
             btn3.Click += btn3_Click;
             // 
             // btnSubtract
             // 
+            btnSubtract.BackColor = Color.Black;
+            btnSubtract.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSubtract.ForeColor = Color.White;
             btnSubtract.Location = new Point(200, 127);
             btnSubtract.Name = "btnSubtract";
             btnSubtract.Size = new Size(53, 35);
             btnSubtract.TabIndex = 16;
             btnSubtract.Text = "-";
-            btnSubtract.UseVisualStyleBackColor = true;
+            btnSubtract.UseVisualStyleBackColor = false;
+            btnSubtract.Click += btnSubtract_Click;
             // 
             // btn0
             // 
+            btn0.BackColor = Color.Black;
+            btn0.Font = new Font("Bell MT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn0.ForeColor = Color.Violet;
             btn0.Location = new Point(23, 250);
             btn0.Name = "btn0";
             btn0.Size = new Size(53, 35);
             btn0.TabIndex = 17;
             btn0.Text = "0";
-            btn0.UseVisualStyleBackColor = true;
+            btn0.UseVisualStyleBackColor = false;
             btn0.Click += btn0_Click;
             // 
             // btnAdd
             // 
+            btnAdd.BackColor = Color.Black;
+            btnAdd.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAdd.ForeColor = Color.White;
             btnAdd.Location = new Point(200, 86);
             btnAdd.Name = "btnAdd";
             btnAdd.Size = new Size(53, 35);
             btnAdd.TabIndex = 18;
             btnAdd.Text = "+";
-            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.UseVisualStyleBackColor = false;
             btnAdd.Click += btnAdd_Click;
             // 
             // btnPeriod
             // 
+            btnPeriod.BackColor = Color.Black;
+            btnPeriod.Font = new Font("Bell MT", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPeriod.ForeColor = Color.Violet;
             btnPeriod.Location = new Point(82, 250);
             btnPeriod.Name = "btnPeriod";
             btnPeriod.Size = new Size(53, 35);
             btnPeriod.TabIndex = 19;
             btnPeriod.Text = ".";
-            btnPeriod.UseVisualStyleBackColor = true;
+            btnPeriod.UseVisualStyleBackColor = false;
             btnPeriod.Click += btnPeriod_Click;
             // 
             // btnEquals
             // 
+            btnEquals.BackColor = Color.Black;
+            btnEquals.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnEquals.ForeColor = Color.White;
             btnEquals.Location = new Point(200, 250);
             btnEquals.Name = "btnEquals";
             btnEquals.Size = new Size(53, 35);
             btnEquals.TabIndex = 20;
             btnEquals.Text = "=";
-            btnEquals.UseVisualStyleBackColor = true;
+            btnEquals.UseVisualStyleBackColor = false;
             btnEquals.Click += btnEquals_Click;
             // 
             // txtViewer
             // 
+            txtViewer.BackColor = Color.Black;
+            txtViewer.Font = new Font("Alexander Grotesk", 8.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtViewer.ForeColor = SystemColors.Window;
             txtViewer.Location = new Point(23, 41);
             txtViewer.Multiline = true;
             txtViewer.Name = "txtViewer";
@@ -266,10 +324,23 @@
             txtViewer.TabIndex = 21;
             txtViewer.TextAlign = HorizontalAlignment.Right;
             // 
+            // txtTitle
+            // 
+            txtTitle.BackColor = Color.Black;
+            txtTitle.Font = new Font("Alexander Grotesk", 8.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtTitle.ForeColor = SystemColors.Window;
+            txtTitle.Location = new Point(23, 9);
+            txtTitle.Name = "txtTitle";
+            txtTitle.Size = new Size(230, 26);
+            txtTitle.TabIndex = 0;
+            txtTitle.Text = "C# Built Calculator";
+            txtTitle.TextAlign = HorizontalAlignment.Center;
+            // 
             // BadCalculator
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             ClientSize = new Size(281, 301);
             Controls.Add(txtViewer);
             Controls.Add(btnEquals);
@@ -301,8 +372,6 @@
         }
 
         #endregion
-
-        private TextBox txtTitle;
         private Button btnHistory;
         private Button btnclearAll;
         private Button btnClearRecent;
@@ -324,5 +393,6 @@
         private Button btnPeriod;
         private Button btnEquals;
         private TextBox txtViewer;
+        private TextBox txtTitle;
     }
 }
--- ICalculator/Form1.cs
+++ ICalculator/Form1.cs
@@ -4,16 +4,16 @@ namespace ICalculator
 {
     public partial class BadCalculator : Form
     {
-        StringBuilder textViewer = new StringBuilder();
-        decimal number1;
-        decimal number2;
-        decimal number3;
-        decimal number4;
+        string input = string.Empty;
+        string operation1 = string.Empty;
+        string operation2 = string.Empty;
+        char operation;
         decimal result;
+
         public BadCalculator()
         {
             InitializeComponent();
-            txtViewer.Text = string.Empty;
+
         }
 
         private void BadCalculator_Load(object sender, EventArgs e)
@@ -24,99 +24,176 @@ namespace ICalculator
         #region NumberButtonInputs
         private void btn0_Click(object sender, EventArgs e)
         {
-            txtViewer.AppendText(btn0.Text);
+            this.txtViewer.Text = "";
+            input += "0";
+            this.txtViewer.Text += input;
         }
 
         private void btn1_Click(object sender, EventArgs e)
         {
-            txtViewer.AppendText(btn1.Text);
+            this.txtViewer.Text = "";
+            input += "1";
+            this.txtViewer.Text += input;
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
-            txtViewer.AppendText(btn2.Text);
+            this.txtViewer.Text = "";
+            input += "2";
+            this.txtViewer.Text += input;
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
-            txtViewer.AppendText(btn3.Text);
+            this.txtViewer.Text = "";
+            input += "3";
+            this.txtViewer.Text += input;
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
-            txtViewer.AppendText(btn4.Text);
+            this.txtViewer.Text = "";
+            input += "4";
+            this.txtViewer.Text += input;
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
-            txtViewer.AppendText(btn5.Text);
+            this.txtViewer.Text = "";
+            input += "5";
+            this.txtViewer.Text += input;
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
-            txtViewer.AppendText(btn6.Text);
+            this.txtViewer.Text = "";
+            input += "6";
+            this.txtViewer.Text += input;
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
-            txtViewer.AppendText(btn7.Text);
+            this.txtViewer.Text = "";
+            input += "7";
+            this.txtViewer.Text += input;
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
-            txtViewer.AppendText(btn8.Text);
+            this.txtViewer.Text = "";
+            input += "8";
+            this.txtViewer.Text += input;
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
-            txtViewer.AppendText(btn9.Text);
+            this.txtViewer.Text = "";
+            input += "9";
+            this.txtViewer.Text += input;
         }
         #endregion
 
+        #region Operations
         private void btnPeriod_Click(object sender, EventArgs e)
         {
-            txtViewer.AppendText(btnPeriod.Text);
+            this.txtViewer.Text = "";
+            input += ".";
+            this.txtViewer.Text += input;
         }
 
         private void btnDeleteDigit_Click(object sender, EventArgs e)
         {
-            txtViewer.Text = txtViewer.Text.Substring(0, txtViewer.Text.Length - 1);
+            string view = this.txtViewer.Text;
+            string dView=view.Substring(0, view.Length - 1);
+            txtViewer.Text = dView;
 
         }
 
         private void btnClearRecent_Click(object sender, EventArgs e)
         {
-            txtViewer.Text = txtViewer.Text.Remove(0);
+            this.txtViewer.Text = "";
+            if (operation1 == string.Empty)
+                this.input = string.Empty;
+            else this.operation1 = string.Empty;
         }
 
         private void btnclearAll_Click(object sender, EventArgs e)
         {
-            txtViewer.Text = string.Empty;
+            this.txtViewer.Text = "";
+            this.input=string.Empty;
+            this.operation1 = string.Empty;
+            this.operation2 = string.Empty;
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-           
-            string num1 = txtViewer.Text;
-            txtViewer.Text = string.Empty;
-            decimal.TryParse(num1, out number1);
-        }
+            operation1 = input;
+            operation = '+';
+            input = string.Empty;
 
+        }
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            operation1 = input;
+            operation = '-';
+            input = string.Empty;
+        }
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            operation1 = input;
+            operation = 'x';
+            input = string.Empty;
+        }
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            operation1 = input;
+            operation = '÷';
+            input = string.Empty;
+        }
+        #endregion
+        #region IMath
         private void btnEquals_Click(object sender, EventArgs e)
         {
-  
-            string num4 = txtViewer.Text;
-            txtViewer.Text = string.Empty;
-            decimal.TryParse(num4, out number4);
-            if (number1 != null)
+            operation2 = input;
+            decimal num1, num2;
+            decimal.TryParse(operation1, out num1);
+            decimal.TryParse(operation2, out num2);
+
+            switch (operation)
             {
-                Math Add1 = new Math();
-                result = Add1.Add(number1, number4);
+                case '+':
+                    Math calc1 = new Math();
+                    result = calc1.Add(num1, num2);
+                    txtViewer.Text = result.ToString();
+                    break;
+
+                case '-':
+                    Math calc2 = new Math();
+                    result = calc2.Subtract(num1, num2);
+                    txtViewer.Text = result.ToString();
+                    break;
+
+                case 'x':
+                    Math calc3 = new Math();
+                    result = calc3.Multiply(num1, num2);
+                    txtViewer.Text = result.ToString();
+                    break;
+
+                case '÷':
+                    Math calc4 = new Math();
+                    if (num2 != 0)
+                    {
+                        result = calc4.Divide(num1, num2);
+                        txtViewer.Text = result.ToString();
+                    }
+                    else txtViewer.Text = "DIVZero";
+                    break;
             }
-            string resultString = result.ToString();
-            txtViewer.Text = resultString;
+
+        }  
+        #endregion
 
 
-        }
+
     }
 }
